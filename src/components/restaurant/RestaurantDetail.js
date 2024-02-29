2aed2ad096da9bc8b7239a7d7a406283eb1f92c4
AlexsHere <ale95.salva@hotmail.it>
2024-02-29 18:04:22 +0100

    Restaurant Detail OK

--- src/components/restaurant/RestaurantDetail.js
+++ src/components/restaurant/RestaurantDetail.js
@@ -2,17 +2,18 @@ import axios from "axios";
 import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { currentRestaurant } from "../../App";
+import { currentDelivery, currentRestaurant, currentUser } from "../../App";
 import MenuForm from "./MenuForm";
 
 export default function RestaurantDetail(props)
 {
-    let {id} = useParams();
+    let {id,uid} = useParams();
 
-    const [user, setUsers] = useState([]);
+    
     const [restaurant, setRestaurant] = useState({});
+    const [user, setUser] = useAtom(currentUser);
+    const [delivery, setDelivery] = useAtom(currentDelivery);
 
-   
 
     useEffect(() => {
         axios.get("/restaurant/"+id).then((response) => {
@@ -21,40 +22,32 @@ export default function RestaurantDetail(props)
         });
     }, []);
 
-    
+    useEffect(() => {
+        axios.get("/createDelivery/"+ uid +"/"+id).then((response) => {
+            setDelivery(response.data);
+
+        });
+    }, []);
+
+   
         return (
-            <div className="row row-cols-md-3 g-4" style={{ marginTop: "0%" }}>
-               
-                   
-                        <div className="card">
-                            <div className="card-body">
-                            <h5 className="card-title">{restaurant.name}</h5>
-                            <span className="input-group-text">Phone</span>
-                                <li className="list-group-item"> {restaurant.phone} </li>
-                            <span className="input-group-text">Opening Hour</span>
-                                <li className="list-group-item"> {restaurant.openingHour} </li>
-                            <span className="input-group-text">Closing Hour</span>
-                                <li className="list-group-item"> {restaurant.closingHour} </li>
-                            <span className="input-group-text">Position X</span>
-                                <li className="list-group-item"> {restaurant.positionX} </li>
-                            <span className="input-group-text">Position Y</span>
-                                <li className="list-group-item"> {restaurant.positionY} </li>
-                                <span className="input-group-text">Food Types</span>
-                                <li className="list-group-item"> {restaurant.foodTypes} </li>
-                            <span className="input-group-text">Delivery Price Per Unit</span>
-                                <li className="list-group-item"> {restaurant.deliveryPricePerUnit} </li>
-                            <span className="input-group-text">Max Delivery Distance</span>
-                                <li className="list-group-item"> {restaurant.maxDeliveryDistance} </li>
-                            <span className="input-group-text">Image</span>
-                                <li className="list-group-item"> {restaurant.imgUrl} </li>
-                            <MenuForm id={id}/>
-                            {/* <span className="input-group-text">Deliveries</span>
-                                <li className="list-group-item"> {restaurant.deliveries} </li> */}
-                            </div>
-                        </div>
-                    </div>
-                
-          
+            <div>
+                <div className="restaurant-info" style={{ border: '2px solid #ccc', padding: '10px', textAlign: 'center', width: '300px', fontFamily: 'Arial, sans-serif' }}>
+                                {/* Contenuto del rettangolo */}
+                                <h5 className="card-title">{restaurant.name}</h5>
+                                <ul className="list-group">
+                                    <li className="list-group-item">Phone: {restaurant.phone}</li> <li className="list-group-item">Opening Hour: {restaurant.openingHour}</li>
+                                    <li className="list-group-item">Closing Hour: {restaurant.closingHour}</li> <li className="list-group-item">Position X: {restaurant.positionX}</li>
+                                    <li className="list-group-item">Position Y: {restaurant.positionY}</li> <li className="list-group-item">Food Types: {restaurant.foodTypes}</li>
+                                    <li className="list-group-item">Delivery Price Per Unit: {restaurant.deliveryPricePerUnit}</li>  <li className="list-group-item">Max Delivery Distance: {restaurant.maxDeliveryDistance}</li>
+                                    <li className="list-group-item">Max Delivery Distance: {restaurant.maxDeliveryDistance}</li>
+                                    <li className="list-group-item">Image: {restaurant.imgUrl}</li>
+                                </ul>
+                </div>
+                    <div className="menu-form">
+                    <MenuForm id={id}/>
+                     </div>
+            </div>
         );
     }
     
